fbd602af88d9371a1d17ca2f0d899c72c351c00c
yyIntsuki <intsuki.yz@gmail.com>
2018-09-18 15:03:17 +0200

    Initial program uploaded.

---
+++ Project/Project/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initial Card Deck in string array.
+            string[,] card_deck = new string[,]
+            {
+                {"S_A", "H_A", "C_A", "D_A"},
+                {"S_2", "H_2", "C_2", "D_2"},
+                {"S_3", "H_3", "C_3", "D_3"},
+                {"S_4", "H_4", "C_4", "D_4"},
+                {"S_5", "H_5", "C_5", "D_5"},
+                {"S_6", "H_6", "C_6", "D_6"},
+                {"S_7", "H_7", "C_7", "D_7"},
+                {"S_8", "H_8", "C_8", "D_8"},
+                {"S_9", "H_9", "C_9", "D_9"},
+                {"S_10", "H_10", "C_10", "D_10"},
+                {"S_J", "H_J", "C_J", "D_J"},
+                {"S_Q", "H_Q", "C_Q", "D_Q"},
+                {"S_K", "H_K", "C_K", "D_K"},
+            };
+
+            Console.ReadLine();
+        }
+    }
+}
